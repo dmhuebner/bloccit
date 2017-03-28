e6144251ade66be9ac9911ade523a3cd98e91f13
David M <davidmh.mail@gmail.com>
2017-03-27 19:46:25 -0600

    Checkpoint 18: Rails: CRUD intro - completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  	resources :posts
 
-  get 'welcome/about'
+  	get 'about' => 'welcome#about'
 
-  get 'welcome/contact'
-
-  root 'welcome#index'
+  	root 'welcome#index'
 end
