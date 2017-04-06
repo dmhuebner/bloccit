b55c41ebbcd53b85036c7175a28e6d0355fc22f0
David M <davidmh.mail@gmail.com>
2017-04-05 18:19:02 -0600

    Checkoint 27: Posts & Users - completed

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,7 +3,8 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
 	let(:comment) {Comment.create!(body: "Comment Body", post: post)}
 	let(:topic) {Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)}
-	let(:post) {topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph)}
+	let(:user) {User.create!(name: "Bloccit User", email: "user@bloccit.io", password: "helloworld")}
+	let(:post) {topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph, user: user)}
 
 	describe "attributes" do
 		it "has a body attribute" do
